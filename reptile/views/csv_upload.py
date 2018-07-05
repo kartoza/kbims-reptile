4273857d7a2b8f373622f8e06149581bd43703c5
Dimas <dimas.ciputra@gmail.com>
2018-07-05 14:34:34 +0700

    Fix count

--- reptile/views/csv_upload.py
+++ reptile/views/csv_upload.py
@@ -108,7 +108,7 @@ class CsvUploadView(FormView):
                                 record[opt_field] = record[opt_field] == '1'
                             optional_records[opt_field] = record[opt_field]
 
-                    collection_records, status = ReptileCollectionRecord.\
+                    collection_records, created = ReptileCollectionRecord.\
                         objects.\
                         get_or_create(
                             site=location_site,
@@ -123,7 +123,7 @@ class CsvUploadView(FormView):
                             **optional_records
                         )
 
-                    if not status:
+                    if created:
                         print('%s Added' % record['species_name'])
                         collection_processed['added'] += 1
 
